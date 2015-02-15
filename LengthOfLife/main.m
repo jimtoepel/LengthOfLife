582f1160a1e70290a7b7def6a835f7d606b7928d
jimtoepel <jim.toepel@gmail.com>
2015-02-15 14:49:03 -0800

    Calculated time and added some flair.

--- LengthOfLife/main.m
+++ LengthOfLife/main.m
@@ -10,8 +10,33 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        // interval since Date.
+        NSDate *now = [[NSDate alloc] init];
+        NSLog(@"\nThe date is: %@", now);
+        
+        
+        // set my date
+        NSDateComponents *comps = [[NSDateComponents alloc] init];
+        [comps setYear: 1982];
+        [comps setMonth: 2];
+        [comps setDay: 18];
+        [comps setHour: 14];
+        [comps setMinute: 32];
+        [comps setSecond: 00];
+        
+        NSCalendar *g = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+        NSDate *dateOfBirth = [g dateFromComponents:comps];
+        
+        NSLog(@"\nMy Birthday is: %@", dateOfBirth);
+        
+        double lifeTime = [now timeIntervalSinceDate:dateOfBirth];
+        
+        NSLog(@"\nI've been alive for approximately %f seconds", lifeTime);
+        
+        float lifeYears = lifeTime/60/60/24/365.25;
+        
+        NSLog(@"\nThat's %.2f years, if you're keeping track...", lifeYears);
     }
     return 0;
 }
